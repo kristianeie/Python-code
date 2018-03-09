44f8977b98ae48aa456d07fd606a61ead48765fd
kristian eie <kmeie1234@gmail.com>
2018-03-09 20:24:41 +0000

    add client_sender method to script bhpnet

--- bhpnet.py
+++ bhpnet.py
@@ -16,13 +16,54 @@ target 				= 	""
 upload_destination	= 	""
 port				= 	0
 
+def client_sender(buffer):
+	
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	try: 
+		# connect to our target host
+		client.connect((target,port))
+		
+		if len(buffer):
+			client.send(buffer)
+		
+		while True:
+			
+			# now wait for data back
+			recv_len = 1
+			response = ""
+			
+			while recv_len:
+				
+				data = client.recv(4096)
+				recv_len = len(data)
+				reponse = response + data
+
+				if recv_len < 4096:
+					break
+		print response
+
+		# wait for more input 
+		buffer = raw_input("")
+		buffer = buffer + "\n"
+
+		# send it off
+		client.send(buffer)
+ 		
+	except:
+		
+		print "[*] Exception! Exiting."
+
+		# tear down the connection
+		client.close()
+	
 def usage():
 	print "BHP Net Tool"
 	print
 	print "Usage: bhpnet.py -t target_host -p port"
 	print "-l --listen					- listen on [host]:[port] for incoming connections"
 	print "-e --execute=file_to_run		- execute the given file upon receiving a connection"
-	print "-c --command					- initialize a command shell"
+	print "-c --commaned					- initialize a command shell"
 	print "-u --upload=destination		- upon receiving a connection upload a file and write to [destination]"
 	
 	print 
@@ -88,3 +129,7 @@ def main():
 		server_loop()
 
 main()
+
+
+
+
