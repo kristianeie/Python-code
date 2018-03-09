f2f478b040556a04f59276e875c3ed337f967183
kristian eie <kmeie1234@gmail.com>
2018-03-09 21:46:03 +0000

    add method server_loop() and run_command() to script bhpnet

--- bhpnet.py
+++ bhpnet.py
@@ -16,6 +16,40 @@ target 				= 	""
 upload_destination	= 	""
 port				= 	0
 
+
+def server_loop():
+	global target
+
+	# if no target is defined, we list the interfaces
+	if not len(target):
+		target = "0.0.0.0"
+
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind((target, port))
+	server.listen(5)
+	
+	while True:
+		client_socket, addr = server.accept()
+		
+		# spin off a thread to handle our new client
+		client_thread = threading.Thread(target=client_handler,args=(client_socket,))
+		client_thread.start()
+
+def run_command(command):
+	
+	# trim the newline
+	command = command.rstrip()
+	
+	# run the command and get the output back
+	try: 
+		output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+	except:
+		output = "Failed to execute command. \r\n"
+
+	# send the output back to the client
+	return output
+
+
 def client_sender(buffer):
 	
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
